cb2bad50a6c15667a7b4dfd03f28ce580d97bc55
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-01-06 16:13:34 -0300

    Add files via upload

---
+++ Primeira/Introducao/intro.js
@@ -0,0 +1,49 @@
+console.log("Ola pessoas")
+
+// VARIAVEIS
+// responsaveis por guardar dados na memoria, evitam repetição
+var nome = "lucas"
+
+console.log(nome)
+
+let idade = 18
+const temCabeloPreto = true
+
+console.log(temCabeloPreto)
+
+// string, boolean, number
+
+console.log(typeof nome, typeof idade, typeof temCabeloPreto)
+
+var preco = 25
+var quantidade = 5
+var total = preco * quantidade
+
+console.log(total)
+
+
+var sobrenome, cidade, ano
+sobrenome = "Pereira"
+cidade = "Curitiba"
+ano = 2025
+console.log(nome, sobrenome, cidade)
+console.log(nome, sobrenome + " de:" + " " + cidade + " no ano de - " + ano)
+
+// nomes podem iniciar com $, _ ou letras
+var Nome = "andre"
+var nome = "josias"
+var lalala
+//concatenação
+lalala = Nome + " " + nome
+
+//template string
+lalala = `${Nome} ${nome}`
+console.log(lalala)
+
+var time = "vasco"
+var time = "flamengo"
+console.log(time, "AQUI É MEU TIME")
+
+
+
+
