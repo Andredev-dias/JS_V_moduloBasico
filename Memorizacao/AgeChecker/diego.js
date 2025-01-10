0662c636c5d371fb563ff22b04672cd427c02159
Andre Dias <andredias.curitiba@gmail.com>
2025-01-10 16:19:34 -0300

    age checker

---
+++ Memorizacao/AgeChecker/diego.js
@@ -0,0 +1,24 @@
+function checkMajority(){
+    var wasBorn = document.getElementById("clientBirth").valueAsNumber
+    var res = document.getElementById("result")
+
+ 
+if (isNaN(wasBorn)) {
+    res.textContent = "Type only numbers 🚫"
+    res.style.color = "red"
+    return
+}
+ 
+const thisYear = newDate().getFullYear()
+const age = thisYear - wasBorn
+ 
+if (age >= 18) {
+    res.textContent = "Over 18, ENTRY ✅"
+    res.style.color = "green"
+    document.getElementById("clientAge").textContent = '${age} = years old'
+} else {
+    res.textContent = "Under 18, NOT ALLOWED 🔞"
+    res.style.color = "orange"
+    document.getElementById("clientAge").textContent = age + " - " + "years old"
+}
+}
---
+++ Memorizacao/AgeChecker/script.js
@@ -0,0 +1,23 @@
+function checkMajority(){
+    var wasBorn = document.getElementById("clientBirth").valueAsNumber
+    var res = document.getElementById("result")
+
+    if(isNaN(wasBorn)){
+        res.textContent = "Type only numbers 🚫"
+        res.style.color = "red"
+        return
+    }
+
+    const thisYear = new Date().getFullYear()
+    const age = thisYear - wasBorn
+   
+    if(age >= 18){
+        res.textContent = "Over 18, ENTRY ✅"
+        res.style.color = "green"
+        document.getElementById("clientAge").textContent = `${age} - years old`
+    }else{
+        res.textContent = "Under 18, NOT ALLOWED ⛔"
+        res.style.color = "orange"
+        document.getElementById("clientAge").textContent = age + " - " + "years old"
+    }
+}
